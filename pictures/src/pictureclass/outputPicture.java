4d44fffd691c54aa61fd0174338a0752f4a6afe6
Kaitlyn <kmvinson@csustudent.net>
2016-11-25 16:59:25 -0500

    picture

--- pictures/src/pictureclass/outputPicture.java
+++ pictures/src/pictureclass/outputPicture.java
@@ -7,21 +7,28 @@ public class outputPicture{
     public String getImage(String scene){
         switch(scene){
             case "title":
-              return "crystal_cavescene";
+              return "crystal_cavescene.png";
+
             case "knight scene one male":
-                return "castle_male";
+                return "castle_male.png";
+
             case "knight scene one female":
-                return "castle_female";
+                return "castle_female.png";
+
             case "knight cave scene male":
-                return "cave_with_CD_male";
+                return "cave_with_CD_male.png";
+
             case "knight cave scene female":
-                return "cave_with_WD_female";
+                return "cave_with_WD_female.png";
+
             case "cave with fae":
-                return "crystal_cavescene_fae";
+                return "crystal_cavescene_fae.png";
+
             case "dying tree scene":
-                return "darkscenetree_fae";
+                return "darkscenetree_fae.png";
+
             case "healthy tree scene":
-                return "lightscenetree_fae";
+                return "lightscenetree_fae.png";
 
             default:
                 return null;
